3ecddb96331afbf86ac6975a57acc901f5711038
Braden Rehbock <bradenrehbock@Bradens-MacBook-Air-2.local>
2026-05-23 13:54:45 -0400

    Added new details to app.py file

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from password_gen import gen_password
+import os
 
 app = Flask(__name__)
+app.secret_key = "your-secret-key-here"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        length = int(request.form.get("length"))
-        use_num = request.form.get("use_num") == "on"
+        length = int(request.form.get("length", 12))
+        use_num = "use_num" in request.form
+
         password = gen_password(length, use_num)
-        return render_template("index.html", password=password)
-    else:
-        return render_template("index.html")
+        session["password"] = password
+
+        return redirect(url_for("home"))
+    
+    password = session.pop("password", "")
+    return render_template("index.html", password=password)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=True)
