cdccf1a71ba57d0e22c16ecfede934437f9c3e80
Braden Rehbock <bradenrehbock@Bradens-MacBook-Air-2.local>
2026-05-23 12:22:07 -0400

    Add Flask web app with dark mode styling

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from password_gen import gen_password
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        length = int(request.form.get("length"))
+        use_num = request.form.get("use_num") == "on"
+        password = gen_password(length, use_num)
+        return render_template("index.html", password=password)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
